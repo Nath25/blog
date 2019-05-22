dc1cb75f61538938dc60c548407843ceddc1ca9f
Nathalie Cortès <natyv225@gmail.com>
2019-05-22 17:24:40 +0200

    entity symfony

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,7 +3,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Article;
 use App\Form\CategoryType;
 use App\Entity\Category;
 use App\Repository\ArticleRepository;
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -112,4 +112,6 @@ class Article
 
         return $this;
     }
+
+
 }
