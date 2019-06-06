4e9d6993cccbc04f6a0f6245b30db8cfb74342df
Nathalie Cortès <natyv225@gmail.com>
2019-06-06 11:55:54 +0200

    modif envoi mail

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -51,10 +51,10 @@ class ArticleController extends AbstractController
             $entityManager->persist($article);
             $entityManager->flush();
 
-            $adrDest  ='natyv225@gmail.com';
+           // $adrDest  ='natyv225@gmail.com';
             $message = (new \Swift_Message('Un nouvel article vient d\'être publié !'))
-                ->setFrom('cn@aol.com')
-                ->setTo($adrDest)
+               // ->setFrom('cn@aol.com')
+                //->setTo($adrDest)
                // ->setBody('Un nouvel article vient d\'être publié sur le blog ! '.$article->getTitle());
 
                 ->setBody($this->renderView('article/email/notification.html.twig',
